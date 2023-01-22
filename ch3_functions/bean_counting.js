dac895ddd97cfbbb3fc24fd1442e23fa6f2c190d
Ardavan Ghaffari <ghaffari.ardavan@gmail.com>
2023-01-22 17:53:05 +0100

    Add chapter 3 solutions

---
+++ ch3_functions/bean_counting.js
@@ -0,0 +1,9 @@
+function countBs(string) {
+  let count = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] == "B") count++;
+  }
+  return count;
+}
+
+console.log(countBs("ABCD"));
